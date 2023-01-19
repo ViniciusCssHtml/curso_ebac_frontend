ba2586dc73037701b52290d7aa2153ba59477165
VINICIUS CARVALHO DE SOUZA <viniciuscarvalhomuay@gmail.com>
2023-01-19 20:18:11 -0300

    adicionando js

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-const label = document.querySelectorAll(".campo");
-console.log(label);
+const campoA = document.querySelector("#campo-a");
+console.log(campoA);
+const campoB = document.querySelector("#campo-b");
+console.log(campoB);
+const btn = document.querySelector("#btn");
+console.log(btn);
+
+btn.addEventListener("click", function (campoA, campoB) {
+  if (campoA < campoB) {
+    return alert("é menor");
+  } else if (campoA === campoB) {
+    return alert("é igual");
+  } else {
+    return alert("é maior");
+  }
+});
