3ef7219e4b11b0a3a9af79625be7cdb5d0079cdb
VINICIUS CARVALHO DE SOUZA <viniciuscarvalhomuay@gmail.com>
2023-01-19 20:29:08 -0300

    ...

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const campoA = document.querySelector("#campo-a");
-console.log(campoA);
+console.log(typeof campoA);
 const campoB = document.querySelector("#campo-b");
 console.log(campoB);
 const btn = document.querySelector("#btn");
 console.log(btn);
 
 btn.addEventListener("click", function (campoA, campoB) {
-  if (campoA < campoB) {
+  if (campoA > campoB) {
     return alert("é menor");
   } else if (campoA === campoB) {
     return alert("é igual");
   } else {
-    return alert("é maior");
+    return alert("é menor");
   }
 });
