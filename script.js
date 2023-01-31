9381a362aab12050539259c99a4958a175c29659
VINICIUS CARVALHO DE SOUZA <viniciuscarvalhomuay@gmail.com>
2023-01-31 16:21:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,1 @@
-const form = document.getElementById("form");
 
-form.addEventListener("submit", function (event) {
-  let formEvalido = false;
-  event.preventDefault();
-
-  const numA = document.getElementById("campo-a").value;
-  const numB = document.getElementById("campo-b").value;
-  const validacao = numB > numA;
-
-  formEvalido = validacao;
-  if (formEvalido) {
-    return alert("Formulário Válido");
-  } else {
-    return alert("Formulário Inválido");
-  }
-});
-
-console.log(form);
