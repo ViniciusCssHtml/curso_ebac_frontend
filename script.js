a9ad1f07547a538d26bcae6a14975369b30e8a43
VINICIUS CARVALHO DE SOUZA <viniciuscarvalhomuay@gmail.com>
2023-01-31 17:05:29 -0300

    projeto em andamento!

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+const form = document.getElementById("form");
+let formEvalido = false;
 
+function validaForm() {
+  let campoA = document.getElementById("campo-a");
+  let campoB = document.getElementById("campo-b");
+  let formEvalido = campoB.value > campoA.value;
+  if (formEvalido) {
+    alert("Está certo!");
+    campoA.value = "";
+    campoB.value = "";
+  } else {
+    alert("Está errado!");
+    campoA.value = "";
+    campoB.value = "";
+  }
+}
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  validaForm();
+});
